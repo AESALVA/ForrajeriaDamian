1638256c3d7744bd5a9e5adaa933b4f5327bdf0c
AESalva <eduardo_salva@hotmail.com>
2023-05-11 23:45:42 -0300

    features navbar responsive and footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/navbar.css';
 import Logo from '../assets/petShopLogo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
+
+    const [menuResponsive, setMenuResponsive] = useState("NavListResponsive");
+    const handleMenu = () => {
+        if (menuResponsive === "NavListResponsive") {
+          setMenuResponsive("NavListResponsiveShow");
+        } else {
+          setMenuResponsive("NavListResponsive");
+        }
+      };
+
   return (
 <>
 <div className="navbar">
@@ -16,7 +26,7 @@ const NavBar = () => {
     <div className='Title'>
         <h1>Tienda de Mascotas Odi</h1>
      </div>
-    <ul>
+    <ul className='ul'>
         <li><NavLink>Inicio</NavLink></li>
         <li><NavLink>Productos</NavLink></li>
         <li><NavLink>Contacto</NavLink></li>
@@ -24,8 +34,15 @@ const NavBar = () => {
         <li><NavLink>Iniciar Sesión</NavLink></li>
     </ul>
     <div className='barsMenu'>
-    <FontAwesomeIcon icon={faBars} />
+    {menuResponsive==='NavListResponsive'?(<FontAwesomeIcon icon={faBars} onClick={handleMenu} />):(<FontAwesomeIcon icon={faX  } onClick={handleMenu} />)} 
     </div>
+    <ul className={menuResponsive}>
+        <li><NavLink>Inicio</NavLink></li>
+        <li><NavLink>Productos</NavLink></li>
+        <li><NavLink>Contacto</NavLink></li>
+        <li><NavLink>Sobre Nosotros</NavLink></li>
+        <li><NavLink>Iniciar Sesión</NavLink></li>
+    </ul>
 </div>
 </>
     )
