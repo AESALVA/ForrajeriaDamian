8310ad53277355eee870535daf6dc492dc6b2ecf
AESalva <eduardo_salva@hotmail.com>
2023-05-16 18:21:39 -0300

    card detail features

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -59,6 +59,48 @@ const ProductDetail = ({ product }) => {
             <NavLink><FontAwesomeIcon icon={faCartShopping} />{' '}Añadir al carrito</NavLink>
           </div>
         </div>
+        <div className="DetailCardMobile">
+        <div className="CardDetail">
+            <h1>{product.title}</h1>
+            <h3>{`Categoria: ${product.category}`}</h3>
+            <h3>{`Sabor: ${product.type}`}</h3>
+            {product.subtitle ? (
+              <>
+                <h3>{`Calidad: ${product.subtitle}`}</h3>
+              </>
+            ) : (
+              <>
+                <h3>Calidad: Regular</h3>
+              </>
+            )}
+            <p>Precio: $ {product.price}</p>
+            
+          </div>
+        <div className="DetailImg">
+            <img src={product.img} alt="detalleArticulo" />
+            <p>
+              {product.disponibility ? (
+                <>Producto Disponible</>
+              ) : (
+                <>Sin Stock</>
+              )}
+            </p>
+          </div>
+          <div className="DetailFooterMobile">
+          <div className="DetailBtn">
+            <NavLink>Comprar</NavLink>
+            <NavLink><FontAwesomeIcon icon={faCartShopping} />{' '}Añadir al carrito</NavLink>
+          </div>
+        </div>
+          <div className="Detalles">
+              <NavLink onClick={ShowDetail}>
+                Mas detalles <FontAwesomeIcon icon={faChevronDown} />
+              </NavLink>  
+              <span className={showDetailOn}>
+                <span className="Description">{product.description}</span>
+              </span>
+            </div>
+        </div>
       </div>
     </div>
   );
