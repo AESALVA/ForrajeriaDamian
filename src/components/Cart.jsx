e0c74ad98f818531b5bc566ad57d558a82d00d4a
AESalva <eduardo_salva@hotmail.com>
2023-05-18 00:05:03 -0300

    cart features

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,53 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/cart.css";
 import { NavLink } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Cart = () => {
-  const DATA = [
+  const Listado = [
     {
+      id: "1",
       name: "Golden",
       description:
         "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
-      quantity: "1",
+      quantity: 1,
       price: 1000,
     },
     {
+      id: "2",
       name: "Royal",
       description:
         "Lorem ipsusdasddasdasdasdasd asdasdaddasdm dolor sit amet coasdasdnsectetur",
-      quantity: "2",
+      quantity: 2,
       price: 500,
     },
     {
+      id: "3",
       name: "Old Prince",
       description:
         "Lorem iasdasdasdapsum dolor sit amet conseasdadaasdadctetur",
-      quantity: "5",
+      quantity: 5,
       price: 700,
     },
     {
+      id: "4",
       name: "Golden",
       description:
         "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
-      quantity: "1",
+      quantity: 1,
       price: 1000,
     },
     {
+      id: "5",
       name: "Golden",
       description:
         "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
-      quantity: "1",
+      quantity: 1,
       price: 1000,
     },
     {
+      id: "6",
       name: "Golden",
       description:
         "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
-      quantity: "1",
+      quantity: 1,
       price: 1000,
     },
   ];
+
+  const [DATA, setDATA] = useState([
+    {
+      id: "1",
+      name: "Golden",
+      description:
+        "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
+      quantity: 1,
+      price: 1000,
+    },
+    {
+      id: "2",
+      name: "Royal",
+      description:
+        "Lorem ipsusdasddasdasdasdasd asdasdaddasdm dolor sit amet coasdasdnsectetur",
+      quantity: 2,
+      price: 500,
+    },
+    {
+      id: "3",
+      name: "Old Prince",
+      description:
+        "Lorem iasdasdasdapsum dolor sit amet conseasdadaasdadctetur",
+      quantity: 5,
+      price: 700,
+    },
+    {
+      id: "4",
+      name: "Golden",
+      description:
+        "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
+      quantity: 1,
+      price: 1000,
+    },
+    {
+      id: "5",
+      name: "Golden",
+      description:
+        "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
+      quantity: 1,
+      price: 1000,
+    },
+    {
+      id: "6",
+      name: "Golden",
+      description:
+        "Lorem ipsuma dolor sasdasdasdadasdit amet conssdasdasdectetur",
+      quantity: 1,
+      price: 1000,
+    },
+  ]);
+
   let TotalPrice = [];
 
   DATA.map((art) => {
@@ -58,6 +117,47 @@ const Cart = () => {
     return total + num;
   };
   const PriceTotal = TotalPrice.reduce(Total);
+
+  const Up = (id) => {
+    const Prod = DATA.find((art) => art.id === id);
+    Prod.quantity += 1;
+    let price = 0;
+    Listado.map((art) => {
+      if (art.id === id) {
+        price = Prod.quantity * art.price;
+      }
+    });
+
+    const aux = [...DATA];
+    aux.map((art) => {
+      if (art.id === id) {
+        art.quantity = Prod.quantity;
+        art.price = price;
+        setDATA(aux);
+      }
+    });
+  };
+
+  const Down = (id) => {
+    const Prod = DATA.find((art) => art.id === id);
+    if (Prod.quantity > 1) {
+      Prod.quantity -= 1;
+      let price = 0;
+      Listado.map((art) => {
+        if (art.id === id) {
+          price = Prod.quantity * art.price;
+        }
+      });
+      const aux = [...DATA];
+      aux.map((art) => {
+        if (art.id === id) {
+          art.quantity = Prod.quantity;
+          art.price = price;
+          setDATA(aux);
+        }
+      });
+    }
+  };
 
   return (
     <div className="ContainerCart">
@@ -77,7 +177,21 @@ const Cart = () => {
               <tr key={i}>
                 <td>{articulo.name}</td>
                 <td className="Description">{articulo.description}</td>
-                <td>{articulo.quantity}</td>
+                <td className="UpDownContainer">
+                  {articulo.quantity}
+                  <span className="UpDown">
+                    <FontAwesomeIcon
+                      className="Btn"
+                      onClick={() => Up(articulo.id)}
+                      icon={faChevronUp}
+                    />
+                    <FontAwesomeIcon
+                      onClick={() => Down(articulo.id)}
+                      className="Btn"
+                      icon={faChevronDown}
+                    />
+                  </span>
+                </td>
                 <td>{articulo.price}</td>
               </tr>
             ))}
