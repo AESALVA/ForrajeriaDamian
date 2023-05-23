0ec7eebab9cd06828bbb794c5c6977938c2a2a33
AESalva <eduardo_salva@hotmail.com>
2023-05-23 15:52:09 -0300

    register features

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,60 +1,138 @@
-import React, { useState } from 'react'
-import '../styles/navbar.css';
-import Logo from '../assets/petShopLogo.png';
+import React, { useState } from "react";
+import "../styles/navbar.css";
+import Logo from "../assets/petShopLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faX, faCartShopping,faUsers, faEnvelope, faUser,faNewspaper,faHouse, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import {faBars,faX,faCartShopping,faUsers,faEnvelope,faUser,faNewspaper,faHouse,faShoppingCart,} from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
-import {
-    useUserContext,
-    useLoadedContext,
-    useValidationContext,
-    useArticlesContext,
-  } from "../UserProvider";
-
+import {useUserContext,useLoadedContext,useValidationContext,useArticlesContext,} from "../UserProvider";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 const NavBar = () => {
-    const Articles = useArticlesContext();
+  const Articles = useArticlesContext();
+  const auth = useUserContext();
 
-
-    const [menuResponsive, setMenuResponsive] = useState("NavListResponsive");
-    const handleMenu = () => {
-        if (menuResponsive === "NavListResponsive") {
-          setMenuResponsive("NavListResponsiveShow");
-        } else {
-          setMenuResponsive("NavListResponsive");
-        }
-      };
+  const [menuResponsive, setMenuResponsive] = useState("NavListResponsive");
+  const handleMenu = () => {
+    if (menuResponsive === "NavListResponsive") {
+      setMenuResponsive("NavListResponsiveShow");
+    } else {
+      setMenuResponsive("NavListResponsive");
+    }
+  };
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Cerrar Sesión
+    </Tooltip>
+  );
 
   return (
-<>
-<div className="navbar">
-    <NavLink className='Logo'>
-        <img src={Logo} alt="Logo" />
-    </NavLink>
-    <div className='Title'>
-        <h1>Tienda de Mascotas Odi</h1>
-     </div>
-    <ul className='ul'>
-        <li><NavLink to='/'>Inicio</NavLink></li>
-        <li><NavLink to='/products'>Productos</NavLink></li>
-        <li><NavLink to='/contact'>Contacto</NavLink></li>
-        <li><NavLink to='/cart'>Carrito{Articles.cart[0]&&(<><span className='CartSpan'>{Articles.cart.length}</span></>)}</NavLink></li>
-        <li><NavLink to='/login' >Iniciar Sesión</NavLink></li>
-    </ul>
-    <div className='barsMenu'>
-    {menuResponsive==='NavListResponsive'?(<FontAwesomeIcon icon={faBars} onClick={handleMenu} />):(<FontAwesomeIcon icon={faX  } onClick={handleMenu} />)} 
-    </div>
-    <ul className={menuResponsive}>
-        <li><NavLink to='/'><FontAwesomeIcon icon={faHouse} />{' '}Inicio</NavLink></li>
-        <li><NavLink to='/products' ><FontAwesomeIcon icon={faNewspaper} />{' '}Productos</NavLink></li>
-        <li><NavLink to='/contact'><FontAwesomeIcon icon={faEnvelope} />{' '}Contacto</NavLink></li>
-        <li><NavLink to='/about'><FontAwesomeIcon icon={faUsers} />{' '}Sobre Nosotros</NavLink></li>
-        <li><NavLink to='/cart'><FontAwesomeIcon icon={faShoppingCart} />{' '}Carrito{Articles.cart[0]&&(<><span className='CartSpan'>{Articles.cart.length}</span></>)}</NavLink></li>
-        <li><NavLink to='/login'><FontAwesomeIcon icon={faUser} />{' '}Iniciar Sesión</NavLink></li>
-    </ul>
-</div>
-</>
-    )
-}
+    <>
+      <div className="navbar">
+        <NavLink className="Logo">
+          <img src={Logo} alt="Logo" />
+        </NavLink>
+        <div className="Title">
+          <h1>Tienda de Mascotas Odi</h1>
+        </div>
+        <ul className="ul">
+          <li>
+            <NavLink to="/">Inicio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Productos</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contacto</NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart">
+              Carrito
+              {Articles.cart[0] && (
+                <>
+                  <span className="CartSpan">{Articles.cart.length}</span>
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            {!auth.auth.name ? (
+              <>
+                <NavLink to="/Login">
+                  Iniciar Sesión
+                </NavLink>
+              </>
+            ) : (
+              <OverlayTrigger
+                key="login"
+                placement="bottom"
+                overlay={renderTooltip}
+               
+              >
+                <NavLink  className="Username" onClick={auth.logout}><FontAwesomeIcon  icon={faUser} />{'  '}{auth.auth.name}</NavLink>
+              </OverlayTrigger>
+            )}
+          </li>
+        </ul>
+        <div className="barsMenu">
+          {menuResponsive === "NavListResponsive" ? (
+            <FontAwesomeIcon icon={faBars} onClick={handleMenu} />
+          ) : (
+            <FontAwesomeIcon icon={faX} onClick={handleMenu} />
+          )}
+        </div>
+        <ul className={menuResponsive}>
+          <li>
+            <NavLink to="/">
+              <FontAwesomeIcon icon={faHouse} /> Inicio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">
+              <FontAwesomeIcon icon={faNewspaper} /> Productos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              <FontAwesomeIcon icon={faEnvelope} /> Contacto
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">
+              <FontAwesomeIcon icon={faUsers} /> Sobre Nosotros
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart">
+              <FontAwesomeIcon icon={faShoppingCart} /> Carrito
+              {Articles.cart[0] && (
+                <>
+                  <span className="CartSpan">{Articles.cart.length}</span>
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li>
+          {!auth.auth.name ? (
+              <>
+                <NavLink to="/Login">
+                  Iniciar Sesión
+                </NavLink>
+              </>
+            ) : (
+              <OverlayTrigger
+                key="login"
+                placement="bottom"
+                overlay={renderTooltip}
+               
+              >
+                <NavLink  className="Username" onClick={auth.logout}><FontAwesomeIcon  icon={faUser} />{'  '}{auth.auth.name}</NavLink>
+              </OverlayTrigger>
+            )}
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,30 +1,135 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/register.css";
-import { NavLink } from "react-router-dom";
-import {faEnvelope,faLock,faEye,faEyeSlash,faUser} from "@fortawesome/free-solid-svg-icons";
+import { NavLink, useNavigate } from "react-router-dom";
+import {
+  faEnvelope,
+  faLock,
+  faEye,
+  faEyeSlash,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import RegistroImg from '../assets/registroImg.png';
-import RegistroImgMobile from '../assets/RegistroImgMobile.png';
+import RegistroImg from "../assets/registroImg.png";
+import RegistroImgMobile from "../assets/RegistroImgMobile.png";
+import {
+  useUserContext,
+  useLoadedContext,
+  useValidationContext,
+} from "../UserProvider";
+import Loader from "./Loader";
 
 const Register = () => {
+  const auth = useUserContext();
+  const navigate = useNavigate();
+
+  const Load = useLoadedContext();
+
+  const Validation = useValidationContext();
+
+  const [password, setPassword] = useState("");
+  const [mail, setMail] = useState("");
+  const [name, setName] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  const [eye, setEye] = useState(false);
+  const [eyeTwo, setEyeTwo] = useState(false);
+  const [showPass, setShowPass] = useState("password");
+  const [showPassTwo, setShowPassTwo] = useState("password");
+
+  const [firstName, setFirstName] = useState(true);
+  const [firstPassword, setFirstPassword] = useState(true);
+  const [firstMail, setFirstMail] = useState(true);
+  const [wrongCredentials, setWrongCredentials] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleEye = () => {
+    if (eye === false) {
+      setEye(true);
+      setShowPass("text");
+    } else {
+      setEye(false);
+      setShowPass("password");
+    }
+  };
+
+  const handleEyeTwo = () => {
+    if (eyeTwo === false) {
+      setEyeTwo(true);
+      setShowPassTwo("text");
+    } else {
+      setEyeTwo(false);
+      setShowPassTwo("password");
+    }
+  };
+
+  const handleClick = () => {
+    Load.setIsLoaded(true);
+    if (
+      Validation.validateName(name) &&
+      Validation.validateMail(mail) &&
+      Validation.validatePassword(password) &&
+      password === confirmPass
+    ) {
+      auth.setAuth({ name: name, role: "user" });
+      auth.addUser({
+        name: name,
+        mail: mail,
+        password: password,
+        role: "user",
+      });
+      navigate("/");
+    } else {
+      setMessage("Debe completar el formulario");
+      Load.setIsLoaded(false);
+    }
+  };
+
+  useEffect(() => {
+    setWrongCredentials("");
+    password !== confirmPass && setWrongCredentials("Contraseñas no coinciden");
+  }, [confirmPass]);
+
+  useEffect(() => {
+    setMessage("");
+  }, [name, mail, password]);
+
+  useEffect(() => {
+    if (auth.auth.role) {
+      navigate("/");
+    }
+  }, [auth.auth]);
+
   return (
     <div className="ContainerRegister">
       <div className="Register">
-      <div className="ImgRegister">
+        <div className="ImgRegister">
           <img src={RegistroImg} alt="registroImg" />
         </div>
         <div className="CardRegister">
           <h2>Registro</h2>
+          <span className="text-danger d-flex justify-content-center">
+          {message}
+        </span>
           <div className="ImgRegisterMobile">
-          <img src={RegistroImgMobile} alt="registroImg" />
-        </div>
+            <img src={RegistroImgMobile} alt="registroImg" />
+          </div>
           <form className="FormRegister">
             <div className="InputRegister">
               <label>
                 {" "}
-                <FontAwesomeIcon icon={faUser} /> Nombre de Usuario
+                <FontAwesomeIcon icon={faUser} /> Nombre de Usuario{" "}
+                {!Validation.validateName(name) && !firstName && (
+                  <span className="text-danger">Completar</span>
+                )}
               </label>
-              <input type="text" required maxLength={50} placeholder="JulioDamian" />
+              <input
+                type="text"
+                value={name}
+                onInput={(e) => setName(e.target.value)}
+                onBlur={() => setFirstName(false)}
+                required
+                maxLength={50}
+                placeholder="JulioDamian"
+              />
               <span className="Conditions">
                 Al menos una letra mayúscula, sin números
               </span>
@@ -32,17 +137,53 @@ const Register = () => {
             <div className="InputRegister">
               <label>
                 {" "}
-                <FontAwesomeIcon icon={faEnvelope} /> Email
+                <FontAwesomeIcon icon={faEnvelope} /> Email{" "}
+                {!Validation.validateMail(mail) && !firstMail && (
+                  <span className="text-danger">Completar</span>
+                )}
               </label>
-              <input type="mail" required maxLength={40} placeholder="Damian@TiendaDeMascotas.com" />
+              <input
+                type="mail"
+                required
+                maxLength={40}
+                placeholder="Damian@TiendaDeMascotas.com"
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
+                onBlur={() => setFirstMail(false)}
+              />
               <span className="Conditions"></span>
             </div>
             <div className="InputRegister">
               <label>
                 {" "}
-                <FontAwesomeIcon icon={faLock} /> Contraseña
+                <FontAwesomeIcon icon={faLock} /> Contraseña{" "}
+                {!Validation.validatePassword(password) && !firstPassword && (
+                  <span className="text-danger">Completar</span>
+                )}
+                <span className="text-danger">{wrongCredentials}</span>
               </label>
-              <input type="password" required maxLength={30} placeholder="Damian9" />
+              <div className="InputRegisterTwo">
+              <input
+                type={showPass}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onBlur={() => setFirstPassword(false)}
+                required
+                maxLength={30}
+                placeholder="Damian9"
+              />
+              <span onClick={handleEye} className="EyeRegister">
+              {!eye ? (
+                <>
+                  <FontAwesomeIcon icon={faEyeSlash} />
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon icon={faEye} />
+                </>
+              )}
+            </span>
+            </div>
               <span className="Conditions">
                 Al menos una letra mayúscula, una minúscula y un número
               </span>
@@ -50,13 +191,41 @@ const Register = () => {
             <div className="InputRegister">
               <label>
                 {" "}
-                <FontAwesomeIcon icon={faLock} /> Repetir Contraseña
+                <FontAwesomeIcon icon={faLock} /> Repetir Contraseña{" "}
+                {!Validation.validatePassword(confirmPass) &&
+                  !firstPassword && (
+                    <span className="text-danger">Completar</span>
+                  )}
+                <span className="text-danger">{wrongCredentials}</span>
               </label>
-              <input type="password" required maxLength={30} placeholder="Damian9" />
+              <div className="InputRegisterTwo">
+              <input
+                type={showPassTwo}
+                value={confirmPass}
+                onChange={(e) => setConfirmPass(e.target.value)}
+                onBlur={() => setFirstPassword(false)}
+                required
+                maxLength={30}
+                placeholder="Damian9"
+              />
+               <span onClick={handleEyeTwo} className="EyeRegister">
+              {!eyeTwo ? (
+                <>
+                  <FontAwesomeIcon icon={faEyeSlash} />
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon icon={faEye} />
+                </>
+              )}
+            </span>
             </div>
-            <NavLink className="RegisterBtn">Registrarse</NavLink>
+            </div>
+            <NavLink onClick={() => handleClick()} className="RegisterBtn">
+              {Load.isLoaded ? <Loader /> : "Registrarse"}
+            </NavLink>
             <div className="LoginRegister">
-              Ya tienes cuenta?<NavLink to='/login'> Iniciar Sesión</NavLink>
+              Ya tienes cuenta?<NavLink to="/login"> Iniciar Sesión</NavLink>
             </div>
           </form>
         </div>
