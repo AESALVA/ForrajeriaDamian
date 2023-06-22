1ed7afba868ff02adf71d2be241e6a8aae658770
AESalva <eduardo_salva@hotmail.com>
2023-06-21 23:45:16 -0300

    features landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,10 +3,52 @@ import "../styles/landing.css";
 import imgUno from "../assets/landing11.png";
 import imgDos from "../assets/landing1.png";
 import imgTres from "../assets/landing3.png";
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../assets/Carousel1.webp';
+
 
 const Landing = () => {
   return (
     <div className="ContainerLanding">
+      <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block carousel"
+          src={img1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 "
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
       <div className="ContainerImg">
         <div className="img">
           <img src={imgTres} alt="landing3" />
